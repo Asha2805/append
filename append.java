b935fdc4b44fa1b425bc86685b54fad6b2848f6f
Asha <43291441+Asha2805@users.noreply.github.com>
2018-10-04 10:17:46 +0530

    Update and rename README.md to append.java

---
+++ append.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class append
+{
+public static void main(String[]args)
+{
+String s;
+Scanner w=new Scanner(System.in);
+s=w.nextLine();
+String e;
+e.append(s);
+System.out.println(e);
+}
+}
